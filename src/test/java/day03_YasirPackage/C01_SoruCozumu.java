b6a2ce9c13d6dd9b9a2005d7751267f16914b39b
yasirdindar <myasirdindar@gmail.com>
2022-12-30 16:37:02 +0300

    day03 grup calismasi tamamlandi

--- src/test/java/day03_YasirPackage/C01_SoruCozumu.java
+++ src/test/java/day03_YasirPackage/C01_SoruCozumu.java
@@ -1,7 +1,55 @@
 package day03_YasirPackage;
 
-public class C01_SoruCozumu {
-    public static void main(String[] args) {
+import com.github.javafaker.Faker;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
 
+public class C01_SoruCozumu extends TestBase {
+    @Test
+    public void test(){
+        //1- https://www.facebook.com adresine gidelim
+        driver.get("https://www.facebook.com");
+
+        //2- Yeni hesap olustur butonuna basalim
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+
+        //3- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim
+        WebElement firsname = driver.findElement(By.xpath("//input[@name='firstname']"));
+
+        Actions actions = new Actions(driver);
+
+        Faker faker = new Faker();
+
+        String email = faker.internet().emailAddress();
+
+        actions.click(firsname)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("25")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Oca")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1995")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .perform();
+
+        //4- Kaydol tusuna basalim
+                driver.findElement(By.xpath("//button[@class='_6j mvm _6wk _6wl _58mi _6o _6v']")).click();
     }
 }
